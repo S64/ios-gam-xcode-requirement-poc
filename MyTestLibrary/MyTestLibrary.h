813a772abe0666ae6c0927a97afe0f0f1b84bf84
Shuma Yoshioka <s.yoshioka@p.cartahd.com>
2025-02-19 16:11:44 +0900

    Initial commit

---
+++ MyTestLibrary/MyTestLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyTestLibrary.h
+//  MyTestLibrary
+//
+//  Created by ce00129 on 2025/02/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyTestLibrary.
+FOUNDATION_EXPORT double MyTestLibraryVersionNumber;
+
+//! Project version string for MyTestLibrary.
+FOUNDATION_EXPORT const unsigned char MyTestLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTestLibrary/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '17.0'
+
+target 'MyTestLibrary' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Google-Mobile-Ads-SDK', '12.0'
+
+  target 'MyTestLibraryTests' do
+    # Pods for testing
+  end
+
+end
